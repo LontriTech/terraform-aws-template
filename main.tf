2bb449a07e05527e18c137d404c16808650116fe
Leonardo Rodrigues de Oliveira <84041478+OLeonardoRodrigues@users.noreply.github.com>
2022-07-20 01:15:06 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 /**
  * # Terraform AWS: Template
  *
- * Terraform AWS Module Template.
+ * Terraform Module Template for AWS.
  *
  * [![Terraform Docs](https://github.com/fr-platform/terraform-aws-template/actions/workflows/terraform-docs.yml/badge.svg)](https://github.com/fr-platform/terraform-aws-template/actions/workflows/terraform-docs.yml)
  * [![Checkov](https://github.com/fr-platform/terraform-aws-template/actions/workflows/checkov.yml/badge.svg)](https://github.com/fr-platform/terraform-aws-template/actions/workflows/checkov.yml)
