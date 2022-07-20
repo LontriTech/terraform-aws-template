10f5143d680aaf7b09a3e7ec58047504d435482b
OLeonardoRodrigues <oliveira.leonardo.r@gmail.com>
2022-07-20 01:12:54 -0300

    initial commit

--- main.tf
+++ main.tf
@@ -1,12 +1,19 @@
 /**
- * # Terraform: Template
+ * # Terraform AWS: Template
  *
- * Terraform Module Template.
+ * Terraform AWS Module Template.
  *
- * [![Terraform Docs](https://github.com/fr-platform/terraform-template/actions/workflows/terraform-docs.yml/badge.svg)](https://github.com/fr-platform/terraform-template/actions/workflows/terraform-docs.yml)
- * [![Checkov](https://github.com/fr-platform/terraform-template/actions/workflows/checkov.yml/badge.svg)](https://github.com/fr-platform/terraform-template/actions/workflows/checkov.yml)
+ * [![Terraform Docs](https://github.com/fr-platform/terraform-aws-template/actions/workflows/terraform-docs.yml/badge.svg)](https://github.com/fr-platform/terraform-aws-template/actions/workflows/terraform-docs.yml)
+ * [![Checkov](https://github.com/fr-platform/terraform-aws-template/actions/workflows/checkov.yml/badge.svg)](https://github.com/fr-platform/terraform-aws-template/actions/workflows/checkov.yml)
  */
 
 terraform {
   required_version = "~> 1.2.0"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.22.0"
+    }
+  }
 }
